7fea709f0e2cf841dc8471abf01f6aae85a4b477
Lola <lorenagimenez1@hotmail.com>
2023-07-30 19:23:53 -0300

    Tipo de datos

--- src/main/java/com/lola/Main.java
+++ src/main/java/com/lola/Main.java
@@ -11,6 +11,44 @@ public class Main {
 
         System.out.println(123.679);
 
+        // declaracion e inicializacion
+        int edad; // declarando la variable de tipo int
+        char fecha;
+        edad = 18; // inicializando la variable edad con valor 18
+        char tipo_documento = 'D' ; // declarar e inicializar una variable
+
+        // tipos primitivos
+        byte var1 = 5;
+        short var2 = 100;
+        int var3 = 10000;
+        long var4 = 100000000L;
+
+        float var5 = 10.12f;
+        double var6 = 10.2;
+
+        char var7 = 't';
+
+        boolean var8 = true;
+        boolean var9 = false;
+
+
+
+        int valor1 = 10;
+        int valor2 = 20;
+
+        int suma = valor1 + valor2;
+
+        System.out.println(suma);
+
+        System.out.println( var5 + var6);
+
+
+        System.out.println( var3 + var5);
+
+
+
+
+
 
 
 
