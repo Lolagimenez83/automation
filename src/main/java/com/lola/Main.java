ec4aec7ca7c089d5ddd8bcaf9d4c2aedce82e2b1
Lola <lorenagimenez1@hotmail.com>
2023-07-30 08:39:18 -0300

    Proyecto Base

---
+++ src/main/java/com/lola/Main.java
@@ -0,0 +1,21 @@
+package com.lola;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!\n");
+
+        System.out.println("Hola Lola");
+
+        System.out.println(123456);
+
+        System.out.println(123.679);
+
+
+
+
+
+
+
+    }
+}
